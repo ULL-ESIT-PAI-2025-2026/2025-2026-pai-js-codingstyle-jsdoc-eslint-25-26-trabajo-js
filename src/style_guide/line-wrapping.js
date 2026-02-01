666da8f2dabbd123fce1b686e451581db2a7c519
Marco Aguiar Álvarez <alu0101620961@ull.edu.es>
2026-02-01 17:10:03 +0000

    Line Wrapping

---
+++ src/style_guide/line-wrapping.js
@@ -0,0 +1,33 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Daniel Martínez Sopo
+ * @author Marco Aguiar Álvarez
+ * @author Diego García Hernández
+ * @since Feb 01 2026
+ */
+
+
+// Correct: breaking the line in asignation and in division.
+let currentEstimate = 
+    calc(currentEstimate + x * currentEstimate) / 
+    2.0;
+
+// Wrong: breaking the line in the middle of ( ... ).
+const currentEstimate2 = calc(currentEstimate + x *
+    currentEstimate) / 2.0;
+
+
+// Correct: Breaking the line AFTER the operator 
+const longString = 'This is a very long sentence ' +
+    'that needs to be split into ' +
+    'multiple parts.';
+
+
+// EXCEPTION: breaking the line after the ')'
+const data = user.getData()
+    .toLowerCase()
+    .split(',');

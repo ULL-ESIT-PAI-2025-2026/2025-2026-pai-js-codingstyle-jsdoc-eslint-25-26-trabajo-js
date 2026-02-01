f7478e3ae14c7dc691fae3c6cfd8663112249349
Marco Aguiar Álvarez <alu0101620961@ull.edu.es>
2026-02-01 17:57:55 +0000

    braces

---
+++ src/style_guide/braces.js
@@ -0,0 +1,34 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Daniel Martínez Sopo
+ * @author Marco Aguiar Álvarez
+ * @author Diego García Hernández
+ * @since Feb 01 2026
+ */
+
+// THIS ISN´T PYTHON!!
+if (user.isAuthenticated)
+  grantAccess();
+  logAccess(); // This will execute always!
+
+// Correct way:
+if (user.isAuthenticated) {
+  grantAccess();
+  logAccess();
+}
+
+// Wrong: for statements need always {}
+for (let i = 0; i < 5; i++) console.log(i);
+
+// Even if it is just one line, always use {}.
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// EXCEPTION: simple if with no else statement 
+if (isEmpty) return null;
+

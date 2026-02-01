447940fc530289df543ff9c0d8143537162d5609
Diegogarhdez <alu0101633732@ull.edu.es>
2026-02-01 18:50:00 +0000

    extender la mantequilla

---
+++ src/examples/spread-operator.js
@@ -0,0 +1,32 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Daniel Martínez Sopo
+ * @author Marco Aguiar Álvarez
+ * @author Diego García Hernández
+ * @since Jan 30 2026
+ * @desc Show how work spread operator
+ *
+ */
+
+'use strict'
+
+function maxWithoutSpread(list) {
+  console.log(Math.max(list));
+}
+
+function maxWtihSpread(list) {
+  console.log(Math.max(...list));
+}
+
+function main() {
+  let list = [0 , 1 , 2 , -1];
+  maxWithoutSpread(list);
+  console.log('--------------------')
+  maxWtihSpread(list);
+}
+
+main();

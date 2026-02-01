836ee59d40c9c14077b08912a0479f1297606f54
Marco Aguiar Álvarez <alu0101620961@ull.edu.es>
2026-02-01 19:10:46 +0000

    Blank lines

---
+++ src/style_guide/vertical-whitespaces.js
@@ -0,0 +1,37 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Daniel Martínez Sopo
+ * @author Marco Aguiar Álvarez
+ * @author Diego García Hernández
+ * @since Feb 01 2026
+ */
+
+
+// Example of a mandatory blank line
+class User {
+  login() {
+    // ...
+  }
+
+  // MANDATORY blank line ⬆️
+  logout() {
+    // ...
+  }
+}
+
+// example NOT TO do
+function calculate() {
+  // NOT HERE!
+  
+  let x = 10;
+  let y = 20;
+ 
+  // OPTIONAL: Logical separation
+  return x + y;
+
+  // NEITHER HERE!
+}

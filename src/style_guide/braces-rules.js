fb05b9662855306d80502ad6237e8dc37d48efe0
Marco Aguiar Álvarez <alu0101620961@ull.edu.es>
2026-02-01 18:22:33 +0000

    Rules for braces

---
+++ src/style_guide/braces-rules.js
@@ -0,0 +1,39 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Daniel Martínez Sopo
+ * @author Marco Aguiar Álvarez
+ * @author Diego García Hernández
+ * @since Feb 01 2026
+ */
+
+/**
+ *  The opening { on the same line as the statement
+ * - The line is broken just after {
+ * - The line is broken just after the last line inside the block
+ * - The line is broken just after }, unless it is a logical continuation, that is, 
+ *   an `else`, `catch`, or `while` statement in a `do-while` statement. If there is one of these, it is left on the 
+ *   same line as }
+ *
+ */
+
+function deployOnFriday() {
+  try {
+    pushToProd();
+
+  } catch (disaster) {
+    blameTheIntern();
+
+  }
+
+  if (bossIsWatching) {
+    pretendToWork();
+
+  } else {
+    watchMemes(); 
+    
+  }
+}

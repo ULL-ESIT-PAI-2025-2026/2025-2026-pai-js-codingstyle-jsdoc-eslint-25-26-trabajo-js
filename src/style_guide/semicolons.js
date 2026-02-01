f5d7e5385da51d00dac7bccfd404ac18b3292768
Marco Aguiar Álvarez <alu0101620961@ull.edu.es>
2026-02-01 15:47:05 +0000

    Correcion semicolons

--- src/style_guide/semicolons.js
+++ src/style_guide/semicolons.js
@@ -1,34 +1,17 @@
 /**
-
  * Universidad de La Laguna
-
  * Escuela Superior de Ingeniería y Tecnología
-
  * Grado en Ingeniería Informática
-
  * Programación de Aplicaciones Interactivas
-
  *
-
  * @author Daniel Martínez Sopo
-
  * @author Marco Aguiar Álvarez
-
  * @author Diego García Hernández
-
  * @since Feb 01 2026
-
  */
 
-  
-  
-
 const name = 'Alex'
-
 const surname = 'Smith';
 
-  
-
 console.log(name)
-
 console.log(surname);

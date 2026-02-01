4a837da1ae405a239da93ac40f7eec5124203135
Diegogarhdez <alu0101633732@ull.edu.es>
2026-02-01 19:55:46 +0000

    estilazo

---
+++ src/examples/JSDoc-style.js
@@ -0,0 +1,42 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Daniel Martínez Sopo
+ * @author Marco Aguiar Álvarez
+ * @author Diego García Hernández
+ * @since Jan 30 2026
+ * @desc Shows how to use JSDoc comments
+ *
+ */
+
+
+/**
+ * @desc This function prints 'Hello, World!'
+ *
+ * @returns 'Hello, World!'
+ */
+function foo() {
+  return 'Hello, World!';
+}
+
+
+/**
+ * @desc This function calculates the square of number
+ *
+ * @param number number to be squared
+ * @returns {number} square of variable number
+ */
+function square(number) {
+  return number * number;
+}
+
+
+function main() {
+  console.log(foo());
+  console.log(square(5));
+}
+
+main();

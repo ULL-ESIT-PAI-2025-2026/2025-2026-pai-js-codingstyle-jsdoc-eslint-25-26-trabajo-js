ace67377ab44cf6faaa2ac1207bde8eb07d5261b
dmtnz <alu0101675893@ull.edu.es>
2026-02-01 18:21:55 +0000

    More examples

--- src/examples/declarative-function.js
+++ src/examples/declarative-function.js
@@ -13,13 +13,8 @@
  */
 
 
-console.log("El futuro dice:", future());
+console.log("Future says:", future());
 
 function future() {
-  return "Nunca tendrás autos voladores";
+  return "You will pass PAI";
 }
-
-/**
- * Explicar que
- * El código anterior funciona, incluso aunque la función esté definida debajo del código que la usa. Las declaraciones de función no forman parte del flujo de control regular de arriba hacia abajo. Conceptualmente se mueven al principio de su alcance y pueden ser utilizadas por todo el código en ese alcance.
- */

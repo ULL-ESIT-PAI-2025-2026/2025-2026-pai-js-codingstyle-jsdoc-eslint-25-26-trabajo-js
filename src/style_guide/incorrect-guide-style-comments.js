0a01c7aa2ab32756411cb80952c4b471f3931b75
Marco Aguiar Álvarez <alu0101620961@ull.edu.es>
2026-02-04 17:19:13 +0000

    minor correction

--- src/style_guide/incorrect-guide-style-comments.js
+++ src/style_guide/incorrect-guide-style-comments.js
@@ -12,17 +12,17 @@
 
 'use strict'
 
-function pow(x,n) //1. Missing space afetr comma, 7. Incorrect variable naming 
+function pow(x,n) //1. Missing space afetr comma, 8. Incorrect variable naming 
 { // 2. Brace is not in the same line as statement
   let result=1; // 3. no space arround '=' 
   for(let i=0;i<n;i++) {result*=x;} // 4. Coding horror
   return result;
 }
 
-let x=prompt("x?",''), n=prompt("n?",'') // 3. no space arround '=' , 5. mutiple declarations in the same line, 7. Incorrect variable naming 
+let x=prompt("x?",''), n=prompt("n?",'') // 3. no space arround '=' , 5. mutiple declarations in the same line, 8. Incorrect variable naming 
 if (n<=0) // 3. no space arround <=
 { // 2. Brace is not in the same line as statement
-console.log(`Power ${n} is not supported, please enter an integer number greater than zero`); //6. No indentation
+console.log(`Power ${n} is not supported, please enter an integer number greater than zero`); //6. No indentation, 7 More than 80 characters in the smae line
 }
 else
 { // 2. Brace is not in the same line as statement
